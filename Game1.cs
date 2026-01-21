77b770abb946884ace2b0ce56692ce31e49d7102
Yuisami <110176625+yuusyaisami@users.noreply.github.com>
2026-01-22 06:47:01 +0900

    roogu v1

--- Game1.cs
+++ Game1.cs
@@ -9,6 +9,10 @@ public class Game1 : Game
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
 
+    Texture2D _player;
+    Vector2 _pos = new(100, 100);
+
+
     public Game1()
     {
         _graphics = new GraphicsDeviceManager(this);
@@ -26,25 +30,48 @@ public class Game1 : Game
     protected override void LoadContent()
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
-
-        // TODO: use this.Content to load your game content here
+        _player = Content.Load<Texture2D>("slime"); // 拡張子不要
     }
+
 
     protected override void Update(GameTime gameTime)
     {
-        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+        var kb = Keyboard.GetState();
+
+        if (kb.IsKeyDown(Keys.Escape))
             Exit();
 
-        // TODO: Add your update logic here
+        float dt = (float)gameTime.ElapsedGameTime.TotalSeconds;
+        float speed = 200f; // 1秒に200px
+
+        Vector2 dir = Vector2.Zero;
+        if (kb.IsKeyDown(Keys.Left)  || kb.IsKeyDown(Keys.A)) dir.X -= 1;
+        if (kb.IsKeyDown(Keys.Right) || kb.IsKeyDown(Keys.D)) dir.X += 1;
+        if (kb.IsKeyDown(Keys.Up)    || kb.IsKeyDown(Keys.W)) dir.Y -= 1;
+        if (kb.IsKeyDown(Keys.Down)  || kb.IsKeyDown(Keys.S)) dir.Y += 1;
+
+        if (dir != Vector2.Zero)
+            dir.Normalize();
+
+        _pos += dir * speed * dt;
 
         base.Update(gameTime);
     }
+
 
     protected override void Draw(GameTime gameTime)
     {
         GraphicsDevice.Clear(Color.CornflowerBlue);
 
-        // TODO: Add your drawing code here
+        _spriteBatch.Begin(
+            samplerState: SamplerState.PointClamp,
+            sortMode: SpriteSortMode.Deferred,
+            blendState: BlendState.AlphaBlend
+        );
+
+        _spriteBatch.Draw(_player, _pos, null, Color.White, 0f, Vector2.Zero, 5.0f, SpriteEffects.None, 0f);
+        _spriteBatch.End();
+
 
         base.Draw(gameTime);
     }
